0db80a8dae30fff44cf3302877dab0f8c44dcab9
vkhonor <61316287+vkhonor@users.noreply.github.com>
2026-04-01 22:42:04 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,3 @@
-sed -i 's/^SRC_VER.*/SRC_VER = 72.9/' padavan-ng/trunk/user/nfqws/Makefile
+sed -i 's/^SRC_VER.*/SRC_VER = 72.12/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
